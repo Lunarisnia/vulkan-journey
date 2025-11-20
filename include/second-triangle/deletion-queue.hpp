25907e8c0b12fc9888660c36e651fe05868a4dad
Lounarisnia <r.arswendo.r@gmail.com>
2025-11-21 01:42:04 +0700

    fix: dequeu

--- include/second-triangle/deletion-queue.hpp
+++ include/second-triangle/deletion-queue.hpp
@@ -1,11 +1,9 @@
 #pragma once
 #include <deque>
 #include <functional>
-#include <vector>
 class DeletionQueue {
  private:
-  /*std::deque<std::function<void()>> deletors;*/
-  std::vector<std::function<void()>> deletors;
+  std::deque<std::function<void()>> deletors;
 
  public:
   void PushFunction(std::function<void()>&& callback);
